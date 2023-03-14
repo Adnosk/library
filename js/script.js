7fac450b431dfada3af5d5155e4eb17f62ba62e6
Adam Novak <adnosk@gmail.com>
2023-03-14 05:43:30 +0100

    Add code for creating table from data in array

--- js/script.js
+++ js/script.js
@@ -16,8 +16,41 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push;
 }
 
-function displayLib {
-  
+function displayLib() {
+  const libraryTable = document.querySelector('#library-table');
+  libraryTable.innerHTML = ''; // Clear the table before adding books
+
+  // Create table header
+  const tableHeader = document.createElement('tr');
+  const headerTitles = ['Title', 'Author', 'Pages', 'Read'];
+  headerTitles.forEach((title) => {
+    const th = document.createElement('th');
+    th.textContent = title;
+    tableHeader.appendChild(th);
+  });
+  libraryTable.appendChild(tableHeader);
+
+  // Create table rows for each book
+  myLibrary.forEach((book) => {
+    const bookRow = document.createElement('tr');
+    const titleCell = document.createElement('td');
+    titleCell.textContent = book.title;
+    bookRow.appendChild(titleCell);
+
+    const authorCell = document.createElement('td');
+    authorCell.textContent = book.author;
+    bookRow.appendChild(authorCell);
+
+    const pagesCell = document.createElement('td');
+    pagesCell.textContent = book.author;
+    bookRow.appendChild(pagesCell);
+
+    const readCell = document.createElement('td');
+    readCell.textContent = book.author;
+    bookRow.appendChild(readCell);
+
+    libraryTable.appendChild(bookRow);
+  });
 }
 
 const theHobbit = new Book( // just some sample data
